eeb2c3e3447f6c2ce661642b7eeb96aa75163eac
paldier <dier0119@gmail.com>
2018-02-09 20:05:18 +0800

    update to V1.0.0.47

--- project/acos/include/ambitCfg.h
+++ project/acos/include/ambitCfg.h
@@ -47,7 +47,7 @@
  
 /*formal version control*/
 #define AMBIT_HARDWARE_VERSION     "U12L358T00"
-#define AMBIT_SOFTWARE_VERSION     "V1.0.0.45"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.0.47"
 #define AMBIT_UI_VERSION           "1.0.1"
 #define STRING_TBL_VERSION         "1.0.0.40_3.1.2.5"
 
